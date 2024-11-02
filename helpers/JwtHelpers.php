736ca9c65db95aa6d9d5b1d416c4997601ec28db
AhmadRizkiadi <rizkiahmad7396@gmail.com>
2024-11-02 20:27:56 +0700

    add handle jwt by user-agent

--- helpers/JwtHelpers.php
+++ helpers/JwtHelpers.php
@@ -25,8 +25,25 @@ class JWTHelper {
         return $jwt;
     }
 
+    private function getJWTFromHeader() {
+        // Implement the logic to get the JWT from the Authorization header
+        if (!isset($_SERVER['HTTP_AUTHORIZATION'])) {
+            throw new Exception('Authorization token not provided.', 401);
+        }
+        
+        $matches = [];
+        if (preg_match('/Bearer (.+)/', $_SERVER['HTTP_AUTHORIZATION'], $matches)) {
+            return $matches[1];
+        } else {
+            throw new Exception('Invalid authorization format.', 401);
+        }
+    }
+
     public function decodeJWT() {
-        $jwt = $this->getJWTFromCookie();
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        $jwt = (isset($userAgent) && strpos($userAgent, 'Mobile') !== false)
+            ? $this->getJWTFromHeader()
+            : $this->getJWTFromCookie();
 
         try {
             return (array) JWT::decode($jwt, new Key($this->jwtSecret, 'HS256'));
@@ -38,16 +55,14 @@ class JWTHelper {
     public function getRoles() {
         $decoded = $this->decodeJWT();
         if (isset($decoded['roles'])) {
-            // Check if roles is an array or a string
             if (is_array($decoded['roles'])) {
-                return array_map('trim', $decoded['roles']); // If it's already an array, just trim whitespace
+                return array_map('trim', $decoded['roles']);
             } elseif (is_string($decoded['roles'])) {
-                return array_map('trim', explode(',', $decoded['roles'])); // Split if it's a string
+                return array_map('trim', explode(',', $decoded['roles']));
             }
         }
-        return []; // Return an empty array if no roles found
+        return [];
     }
-    
 
     public function getUserId() {
         $decoded = $this->decodeJWT();
@@ -58,3 +73,4 @@ class JWTHelper {
         return $this->decodeJWT();
     }
 }
+
