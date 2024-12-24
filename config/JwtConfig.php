db9459dfdec0573ee8f96b9e1b139c237f26da5b
AhmadRizkiadi <rizkiahmad7396@gmail.com>
2024-12-24 17:49:35 +0700

    jwt expiration time change to 1 day

--- config/JwtConfig.php
+++ config/JwtConfig.php
@@ -1,5 +1,5 @@
 <?php
     define('JWT_SECRET', 'pblpolivent'); // Change this to a secure key
-    define('JWT_EXPIRATION_TIME', 3600); // 1 hour for access token
+    define('JWT_EXPIRATION_TIME', 3600 * 24); // 1 hour for access token
     define('JWT_REFRESH_EXPIRATION_TIME', 604800); // 7 days for refresh token
 ?>
